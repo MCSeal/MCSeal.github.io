d5da70cf8e52b275999e22c78b0ebb3bc8b32bdd
MCSeal <mzeal93@gmail.com>
2022-02-15 15:21:37 -0500

    added font awesome icons

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import style from './Projects.css';
 import projectsList from '../../data/projectsList.js';
-
 import { animated, useSprings } from 'react-spring';
-import pic from './pic.jpg';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 const Projects = () => {
   const [index, setIndex] = useState(null);
   const [isDelayed, setIsDelayed] = useState(true);
@@ -11,7 +13,7 @@ const Projects = () => {
   const projs = useSprings(
     projectsList.length,
     projectsList.map((item, i) => ({
-      delay: isDelayed ? 1250 * i : 0,
+      delay: isDelayed ? 250 * i : 0,
       opacity: 1.0,
       transform: 'translateY(0px)',
       overlayOpacity: i === index ? 0 : 1,
@@ -64,9 +66,19 @@ const Projects = () => {
                   class="projects-animated__box"
                   style={{ opacity: textOpacity }}
                 >
-                  <animated.div fontSize={3} fontWeight={2}>
+                  <animated.div
+                    class="projects-title__parent"
+                    fontSize={3}
+                    fontWeight={2}
+                  >
                     <a href={projectsList[i].link} target="_blank">
                       <h2>{projectsList[i].title}</h2>
+                    </a>
+                    <a href={projectsList[i].github} target="_blank">
+                      <FontAwesomeIcon
+                        class="projects-title__child"
+                        icon={faGithub}
+                      />
                     </a>
                   </animated.div>
                   {projectsList[i].description}
